15b7fc58cb5e1bfbd29aa7d2e87d1abd81800b71
Mengyang Liu <myliu@seu.edu.cn>
2023-05-30 08:50:30 +0800

    Bug fixed

--- pack_data.sh
+++ pack_data.sh
@@ -1,5 +1,3 @@
-export DATA_VERSION=0_ori
-
 export DATA_DIR=/mnt/e/Workspace/sky/data/$DATA_VERSION
 
 export BOX_DIR=$DATA_VERSION/box
@@ -10,4 +8,4 @@ cp -r $DATA_DIR/Camera/object_detection $BOX_DIR
 
 cd $DATA_VERSION
 
-zip box.zip box
+zip -r box.zip box
